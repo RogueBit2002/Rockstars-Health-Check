4b87aeaccf6094a3fe4bf3ecece75a148cb89415
WouterVerschuren123 <79634805+WouterVerschuren123@users.noreply.github.com>
2022-09-27 09:34:12 +0200

    wow! dat is een mooie layout!

--- Rockstars Health Check/wwwroot/js/site.js
+++ Rockstars Health Check/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+$(window).scroll(function () {
+    if($(this).scrollTop() > 40){
+        $('#topBtn').fadeIn();
+    } else {
+        $('#topBtn').fadeOut();
+    }
+});
+
+$("#topBtn").click(function () {
+    $('html , body').animate({ scrollTop: 0 }, 800);
+});
+});
+
+$("#linksbutton").click(function () {
+    $('html , body').animate({ scrollTop: 100 }, 400);
+});
+
+
+
